92857f703ac67d147a4b654b04d412c4cfb92755
Nicolas Andrade <nicolasazevedo.an@gmail.com>
2026-06-19 02:02:49 -0300

    Cria repository de pedidos

---
+++ app/src/main/java/com/example/ordermanager/data/repository/OrderRepository.kt
@@ -0,0 +1,23 @@
+package com.example.ordermanager.data.repository
+
+import com.example.ordermanager.data.dao.OrderDao
+import com.example.ordermanager.data.entity.OrderEntity
+
+class OrderRepository(
+    private val orderDao: OrderDao
+) {
+
+    val orders = orderDao.getAllOrdersWithDetails()
+
+    suspend fun insert(order: OrderEntity) {
+        orderDao.insert(order)
+    }
+
+    suspend fun update(order: OrderEntity) {
+        orderDao.update(order)
+    }
+
+    suspend fun delete(order: OrderEntity) {
+        orderDao.delete(order)
+    }
+}
